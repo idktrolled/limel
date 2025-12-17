81eb469da8333e2a0d0f131155cb74b2e147e700
idktrolled <camilocarlos1100@gmail.com>
2025-12-17 17:47:59 -0500

    Update DefineMacro.hx

--- src/lime/_internal/macros/DefineMacro.hx
+++ src/lime/_internal/macros/DefineMacro.hx
@@ -79,9 +79,6 @@ class DefineMacro
 				}
 			}
 		}
-
-		if (Context.defined("android") && Context.defined("extension-androidtools"))
-			Context.fatalError('You cannot add extension-androidtools to this Lime because it is already included. Please remove it from project file.', (macro null).pos);
 	}
 }
 #end
